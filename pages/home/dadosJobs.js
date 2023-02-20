b485421277836580f5d3d5e077bf8503e08d2150
Lucas Maclean <contatolucasmaclean@gmail.com>
2023-02-20 15:22:00 -0300

    Ajustando vagas

--- pages/home/dadosJobs.js
+++ pages/home/dadosJobs.js
@@ -1,29 +1,29 @@
-const jobsData = [
-  {
-    id: 0,
-    title: "Pessoa desenvolvedora front-end - React",
-    enterprise: "Kenzie",
-    location: "Curitiba",
-    descrition:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
-    modalities: ["Hibrido", "Presencial"],
-  },
-  {
-    id: 1,
-    title: "Pessoa desenvolvedora back-end - Node JS",
-    enterprise: "Brazilians in Tech",
-    location: "Rio de Janeiro",
-    descrition:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
-    modalities: ["Home Office", "Presencial"],
-  },
-  {
-    id: 2,
-    title: "Pessoa desenvolvedora Fullstack - Node JS",
-    enterprise: "Brazilians in Tech",
-    location: "Rio de Janeiro",
-    descrition:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
-    modalities: ["Home Office", "Presencial"],
-  },
-];
+// const jobsData = [
+//   {
+//     id: 0,
+//     titulo: "Desenvolvedor(a) back-end - Python",
+//     empresa: "DNC",
+//     localizacao: "Fortaleza",
+//     descricao:
+//       "Gerar um guia de implementação em FHIR a partir desse modelo lógico e construir um repositório FHIR que receba esses dados e se mantenha em conformidade com a especificação que será produzida",
+//     modo: ["Remote", "Hybrid"],
+//   },
+//   {
+//     id: 1,
+//     titulo: "Desenvolvedor(a) FullStack - Node.js",
+//     empresa: "Escola DNC",
+//     localizacao: "São Paulo",
+//     descricao:
+//       "Esta é uma oportunidade de trabalho totalmente remoto em tempo integral, na qual você trabalhará como parte de uma equipe Scrum, que exige trabalhar em estreita colaboração com outros engenheiros de software, partes interessadas e colaboradores do projeto.",
+//     modo: ["Remote", "Presencial"],
+//   },
+//   {
+//     id: 2,
+//     titulo: "Desenvolvedor Front-end Jr. - React.js | Tailwind",
+//     empresa: "Imperium Tech",
+//     localizacao: "Mato Grosso",
+//     descricao:
+//       "Você já é um profissional junior ou pleno no desenvolvimento em React, dificilmente fica trancado para escopos simples, escreve testes automatizados, faz pushes diários e sabe balancear avanço com a refatoração do código.",
+//     modo: ["Remote", "Hybrid"],
+//   },
+// ];
--- pages/home/renderUs.js
+++ pages/home/renderUs.js
@@ -20,16 +20,74 @@
 
 
 })();
-
-
+  
 
 // Criação da session Vagas
 
-(function () {
-    const body = document.body
-    const section = document.createElement('section')
+(function (objeto) {
 
 
-    body.appendChild(section)
+    const body = document.body;
+    const section = document.createElement('section');
+    const section__h1 = document.createElement('h1');
+    const section__div = document.createElement('div');
+    const section__div__h1 = document.createElement('h1');
+    const section__div__div = document.createElement('div');
+    const section__div__div__h4 = document.createElement('h4');
+    const section__div__div__h4_2 = document.createElement('h4');
+    const section__div__p = document.createElement('p');
+    const section__div__div__2 = document.createElement('div');
+    const section__div__button = document.createElement('button');
+    
 
-})()
+
+    body.appendChild(section).append(section__h1, section__div);
+    section__div.append(section__div__h1, section__div__div, section__div__p, section__div__div__2, section__div__button);
+    section__div__div.append(section__div__div__h4, section__div__div__h4_2);
+
+    section__div.setAttribute("id", "box");
+    
+
+    section__h1.innerText = 'Vagas';
+    section__div__h1.innerText = objeto.titulo;
+
+    // Renderização dos Dados do Array de Objetos em Tela
+
+    const jobsData = [
+        {
+          id: 0,
+          titulo: "Desenvolvedor(a) back-end - Python",
+          empresa: "DNC",
+          localizacao: "Fortaleza",
+          descricao:
+            "Gerar um guia de implementação em FHIR a partir desse modelo lógico e construir um repositório FHIR que receba esses dados e se mantenha em conformidade com a especificação que será produzida",
+          modo: ["Remote", "Hybrid"],
+        },
+        {
+          id: 1,
+          titulo: "Desenvolvedor(a) FullStack - Node.js",
+          empresa: "Escola DNC",
+          localizacao: "São Paulo",
+          descricao:
+            "Esta é uma oportunidade de trabalho totalmente remoto em tempo integral, na qual você trabalhará como parte de uma equipe Scrum, que exige trabalhar em estreita colaboração com outros engenheiros de software, partes interessadas e colaboradores do projeto.",
+          modo: ["Remote", "Presencial"],
+        },
+        {
+          id: 2,
+          titulo: "Desenvolvedor Front-end Jr. - React.js | Tailwind",
+          empresa: "Imperium Tech",
+          localizacao: "Mato Grosso",
+          descricao:
+            "Você já é um profissional junior ou pleno no desenvolvimento em React, dificilmente fica trancado para escopos simples, escreve testes automatizados, faz pushes diários e sabe balancear avanço com a refatoração do código.",
+          modo: ["Remote", "Hybrid"],
+        },
+      ];
+
+
+      jobsData.forEach((objeto) => {
+
+        section__div__h1.innerHTML = objeto.empresa
+
+      });
+
+})();
